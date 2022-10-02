0b99fa94a04bd6e7cdd40fafdc72d2fd4ac96661
astrobuddha <jozefleo@protonmail.com>
2022-10-02 14:39:58 -0400

    finished structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,7 +2,6 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 struct ColorClassicStruct {
     red: i32,
@@ -23,9 +22,9 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         let green = ColorClassicStruct {
-            red = 0,
-            green = 255,
-            blue = 0 
+            red: 0,
+            green: 255,
+            blue: 0 
         };
 
         assert_eq!(green.red, 0);
@@ -46,7 +45,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
-        let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
