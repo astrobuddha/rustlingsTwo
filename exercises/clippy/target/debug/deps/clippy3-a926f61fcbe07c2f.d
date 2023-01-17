3159621684c59a8f9a933b691fc826385282b238
astrobuddha <jozefleo@protonmail.com>
2023-01-16 19:31:12 -0500

    clippys and some other stuff

---
+++ exercises/clippy/target/debug/deps/clippy3-a926f61fcbe07c2f.d
@@ -0,0 +1,7 @@
+/mnt/c/myDev/rustlings/exercises/clippy/target/debug/deps/clippy3-a926f61fcbe07c2f.rmeta: clippy3.rs
+
+/mnt/c/myDev/rustlings/exercises/clippy/target/debug/deps/clippy3-a926f61fcbe07c2f.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
