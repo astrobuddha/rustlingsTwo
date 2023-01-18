2c6c408ebfb9b3bff680e32f72dd6cb8f80643ab
astrobuddha <jozefleo@protonmail.com>
2023-01-17 19:26:32 -0500

    finshed try from into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,8 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -48,17 +46,23 @@ impl TryFrom<(i16, i16, i16)> for Color {
         //     return Err(IntoColorError::IntConversion);
         // }
 
-        let toRed: u8;
-        let toGreen: u8;
-        let toBlue: u8;
+        // let toRed: u8;
+        // let toGreen: u8;
+        // let toBlue: u8;
+        // Ok(Self {
+        //     red: u8::try_from(tuple.0).unwrap(),
+        //     green: u8::try_from(tuple.1).unwrap(),
+        //     blue: u8::try_from(tuple.2).unwrap()
+        // })
 
+        let (r, g, b) = (u8::try_from(tuple.0).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(tuple.1).or(Err(IntoColorError::IntConversion))?,
+            u8::try_from(tuple.2).or(Err(IntoColorError::IntConversion))?);
 
-
-        Ok(Self {
-            red: u8::try_from(tuple.0).unwrap(),
-            green: u8::try_from(tuple.1).unwrap(),
-            blue: u8::try_from(tuple.2).unwrap()
-        })
+        Ok(Color {
+            red: r,
+            green: g,
+            blue: b})
     }
 }
 
@@ -66,7 +70,9 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
-        todo!();
+        let [r, g, b] = arr;
+
+        (r, g, b).try_into()
     }
 }
 
@@ -74,7 +80,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
-        todo!();
+
+        match slice {
+            [r, g, b] => Color::try_from((*r, *g, *b)),
+            _ => Err(IntoColorError::BadLen)
+        }
     }
 }
 
